75511039be8363a35dd46d3baf81f383bb788057
Nubia Garcia <nubiagarcia1909@gmail.com>
2026-07-01 17:50:52 -0300

    Added new submit scripts in app.nifi scratch dir

---
+++ scripts_app_nifi_apollo_cluster/HIPS_CATALOG_PIPELINE/submit_hips_des.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -p cpu
+#SBATCH --mem-per-cpu=8G
+#SBATCH -J nifi-hipscatalog-des
+#SBATCH --chdir=/scratch/users/app.nifi
+#SBATCH --output=/scratch/users/%u/hips_catalog/logs/%x-%j.out
+#SBATCH --error=/scratch/users/%u/hips_catalog/logs/%x-%j.err
+
+# -------------------
+# SETUP VARIAVEIS 
+# -------------------
+
+DATARELEASE=DES
+
+BASE_DIR=/scratch/users/app.nifi/hips_catalog
+SCRIPTS_DIR=/scripts/app.nifi/HIPS_CATALOG_PIPELINE
+ENV_DIR=/scripts/app.nifi/HIPS_CATALOG_PIPELINE/.env
+
+JOB_DIR=$BASE_DIR/logs/$DATARELEASE-JOB-${SLURM_JOB_ID}
+mkdir -p "$JOB_DIR"
+
+#-------------------
+# INITIAL REPORT
+#-------------------
+
+$SCRIPTS_DIR/report_scripts/write_initial_report.sh "$JOB_DIR"
+
+# ---------------------------------------------------------
+# ACTIVATION ENV AND EXECUTE PIPELINE
+# ---------------------------------------------------------
+source /opt/conda/etc/profile.d/conda.sh
+conda activate $ENV_DIR/hips-env
+
+EXEC=$SCRIPTS_DIR/pipeline_exec.sh
+
+srun $EXEC
+PIPELINE_RC=$?
+
+# ------------------
+# FINAL REPORT
+# ------------------
+
+$SCRIPTS_DIR/report_scripts/write_final_report.sh \
+    "$JOB_DIR" \
+    "$PIPELINE_RC"
+
+exit $PIPELINE_RC
---
+++ scripts_app_nifi_apollo_cluster/SCIENCE_CATALOG_PIPELINE/submit_science_template.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -p cpu
+#SBATCH --mem-per-cpu=16G
+#SBATCH -J nifi-scienceclgs_<DATARELEASE>
+#SBATCH --chdir=/scratch/users/app.nifi
+#SBATCH --output=/scratch/users/%u/science_catalog/logs/%x-%j.out
+#SBATCH --error=/scratch/users/%u/science_catalog/logs/%x-%j.err
+
+# --------------------------
+# SETUP VARIAVEIS
+# --------------------------
+
+DATARELEASE=teste
+
+BASE_DIR=/scratch/users/app.nifi/science_catalog
+SCRIPTS_DIR=/scripts/app.nifi/SCIENCE_CATALOG_PIPELINE
+OUTPUT_DIR="$BASE_DIR/nifi_scienceclgs_$DATARELEASE"
+
+
+JOB_DIR=$BASE_DIR/logs/$DATARELEASE-JOB-${SLURM_JOB_ID}
+mkdir -p "$JOB_DIR"
+
+
+#--------------------------
+# INITIAL REPORT
+#--------------------------
+
+$SCRIPTS_DIR/report_scripts/write_initial_report.sh "$JOB_DIR" "$OUTPUT_DIR"
+
+# -----------------------------------
+# ACTIVATION ENV AND EXECUTE PIPELINE
+# -----------------------------------
+
+export MICROMAMBA_BIN=/scratch/users/app.nifi/science_catalog/bin/micromamba
+
+EXEC=$BASE_DIR/science_catalogs_core/run.sh
+
+srun $EXEC $BASE_DIR/science_catalogs_core/config.yaml $OUTPUT_DIR
+PIPELINE_RC=$?
+
+# -----------------------
+# FINAL REPORT
+# ----------------------
+
+$SCRIPTS_DIR/report_scripts/write_final_report.sh "$JOB_DIR" "$OUTPUT_DIR" "$PIPELINE_RC"
+
+exit $PIPELINE_RC
